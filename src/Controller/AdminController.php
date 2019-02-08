76a3254c7c716670c9b28516ba3fad45885ca071
linux <carlitos_9ro@hotmail.com>
2019-02-08 16:10:09 +0100

    Primer push

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: linux
+ * Date: 07/02/19
+ * Time: 16:15
+ */
+
+namespace App\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class AdminController
+ * @package App\Controller
+ * @IsGranted("ROLE_ADMIN")
+ *
+ */
+class AdminController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="app_admin")
+     */
+    public function index()
+    {
+        return $this->render('admin/index.html.twig');
+    }
+}
+?>
